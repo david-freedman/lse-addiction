cc908948af1179f5c493c5123039f334bc81e18a
Andriy Zhovna <an.zhovna@gmail.com>
2025-10-29 15:38:22 +0200

    Project cleanup

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <p class="mt-4">Добро пожаловать, {{ Auth::user()->name  ?? 'Гость' }}!</p>
-    </div>
-@endsection
